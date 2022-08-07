0d4d3698b5518aa048c7d52198a8d1e7c0d36bd3
etnavyguy <etnavyguy@gmail.com>
2022-08-07 06:05:55 -0500

    2 Done?

---
+++ src/main/java/Classes/Main.java
@@ -0,0 +1,9 @@
+package Classes;
+
+import com.example.waterplantcalculator.WaterPlantCalc;
+
+public class Main {
+    public static void main(String[] args) {
+        WaterPlantCalc.main(args);
+    }
+}
